8480ce765d6d80bef103241f92e75bedf5dcd294
i-webtest <stasbass80@gmail.com>
2022-03-26 16:22:42 +0300

    правки

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,13 +1,24 @@
 "use strict";
 
 const calculator = () => {
-  const calcItems = document.querySelectorAll("input.calc-item");
+  // const calcItems = document.querySelectorAll("input.calc-item");
+  const calcItems = document.querySelectorAll("input[type=text].calc-item");
 
-  calcItems.forEach((item) => {
-    item.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(/\D+/, "");
-    });
-  });
+  // console.log(calcItems);
+
+  // calcItems.forEach((item) => {
+  //   if (!item.classList.contains("calc-type")) {
+  //     item.addEventListener("input", (e) => {
+  //       e.target.value = e.target.value.replace(/\D+/gi, "");
+  //     });
+  //   }
+  // });
+
+  // calcItems.forEach((item) => {
+  //   item.addEventListener("input", (e) => {
+  //     e.target.value = e.target.value.replace(/\D+/gi, "");
+  //   });
+  // });
 };
 
 export default calculator;
--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,19 +1,30 @@
 "use strict";
 
 const validation = () => {
-  const form = document.querySelectorAll("form");
+  // const form = document.querySelectorAll("form");
   const inputUserName = document.querySelectorAll("input[type=text]");
+  const calcItems = document.querySelectorAll("input[type=text].calc-item");
+  // const inputUserName = document.querySelectorAll("input[name=user_name]");
+  // const inputUserName = document.querySelectorAll(".top-form");
   const inputMessage = document.querySelector(".mess");
   const inputEmail = document.querySelectorAll("input[type=email");
   const inputTel = document.querySelectorAll("input[type=tel]");
 
-  // console.log(inputTel);
+  // console.log(inputUserName);
 
   inputUserName.forEach((item) => {
-    item.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, "");
-      // console.log(e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, ""));
-    });
+    if (item.classList.contains("calc-item")) {
+      item.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/\D+/gi, "");
+      });
+    } else {
+      item.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, "");
+      });
+    }
+    // item.addEventListener("input", (e) => {
+    //   e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, "");
+    // });
   });
 
   inputMessage.addEventListener("input", (e) => {
@@ -29,7 +40,7 @@ const validation = () => {
 
   inputTel.forEach((item) => {
     item.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(/[^\d()-]+/gi, "");
+      e.target.value = e.target.value.replace(/[^\d+()-]+/gi, "");
       // console.log(e.target.value.replace(/[^\d()-]+/gi, ""));
     });
   });
