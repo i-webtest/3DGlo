dc2787f9cde499b2350d141ce46382a9f437a1c3
i-webtest <stasbass80@gmail.com>
2022-03-26 21:35:14 +0300

    правки

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,16 +1,11 @@
 "use strict";
 
 const validation = () => {
-  // const form = document.querySelectorAll("form");
   const inputUserName = document.querySelectorAll("input[type=text]");
   const calcItems = document.querySelectorAll("input[type=text].calc-item");
-  // const inputUserName = document.querySelectorAll("input[name=user_name]");
-  // const inputUserName = document.querySelectorAll(".top-form");
   const inputMessage = document.querySelector(".mess");
   const inputEmail = document.querySelectorAll("input[type=email");
   const inputTel = document.querySelectorAll("input[type=tel]");
-
-  // console.log(inputUserName);
 
   inputUserName.forEach((item) => {
     if (item.classList.contains("calc-item")) {
@@ -22,9 +17,6 @@ const validation = () => {
         e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, "");
       });
     }
-    // item.addEventListener("input", (e) => {
-    //   e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]+/i, "");
-    // });
   });
 
   inputMessage.addEventListener("input", (e) => {
@@ -34,14 +26,12 @@ const validation = () => {
   inputEmail.forEach((item) => {
     item.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^\w@\-.!~*']+/gi, "");
-      // console.log(e.target.value.replace(/[^\w@\-.!~*']+/gi, ""));
     });
   });
 
   inputTel.forEach((item) => {
     item.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^\d+()-]+/gi, "");
-      // console.log(e.target.value.replace(/[^\d()-]+/gi, ""));
     });
   });
 };
