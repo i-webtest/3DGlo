766c2c1457116c3119a488e54106243250509f80
i-webtest <stasbass80@gmail.com>
2022-03-29 18:35:35 +0300

    правки

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -10,42 +10,45 @@ const modal = () => {
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
-      // let count = 0;
-      // let idInterval;
+      let count = 0;
+      let idInterval;
 
       modal.style.display = "block";
-      // popupContent.style.top = -50 + "%";
+      popupContent.style.top = -50 + "%";
 
-      if (screen > 768) {
-        animate({
-          duration: 500,
-          timing(timeFraction) {
-            return timeFraction;
-          },
-          draw(progress) {
-            popupContent.style.top = 30 * progress + "%";
-          },
-        });
-      } else {
-        modal.style.display = "block";
-        popupContent.style.top = 20 + "%";
-      }
+      // if (screen > 768) {
+      //   animate({
+      //     duration: 500,
+      //     timing(timeFraction) {
+      //       return timeFraction;
+      //     },
+      //     draw(progress) {
+      //       popupContent.style.top = 30 * progress + "%";
+      //     },
+      //   });
+      // } else {
+      //   modal.style.display = "block";
+      //   popupContent.style.top = 20 + "%";
+      // }
 
-      /*
       const myAnimate = (e) => {
         count++;
         idInterval = requestAnimationFrame(myAnimate);
 
-        animate({
-          duration: 500,
-          timing(timeFraction) {
-            return timeFraction;
-          },
-          draw(progress) {
-            popupContent.style.top = 50 * progress + "%";
-          },
-        });
-
+        if (count < 25) {
+          animate({
+            duration: 500,
+            timing(timeFraction) {
+              return timeFraction;
+            },
+            draw(progress) {
+              popupContent.style.top = -50 + 50 * progress + count + "%";
+            },
+          });
+          // popupContent.style.top = -50 + count * 3 + "%";
+        } else {
+          cancelAnimationFrame(idInterval);
+        }
 
         // if (count < 25) {
         //   popupContent.style.top = -50 + count * 3 + "%";
@@ -60,7 +63,6 @@ const modal = () => {
       };
 
       myAnimate();
-      */
     });
   });
 
